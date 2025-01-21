62d734d3849dbb52e37082679caf805408080ebc
gagliathebuilder <chrisgaglia@gmail.com>
2025-01-21 09:11:46 -0800

    Initial Commit

---
+++ Sales AiGent/Sales_AiGentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sales_AiGentApp.swift
+//  Sales AiGent
+//
+//  Created by Christopher Gaglia on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Sales_AiGentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sales AiGentTests/Sales_AiGentTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sales_AiGentTests.swift
+//  Sales AiGentTests
+//
+//  Created by Christopher Gaglia on 1/21/25.
+//
+
+import Testing
+@testable import Sales_AiGent
+
+struct Sales_AiGentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
